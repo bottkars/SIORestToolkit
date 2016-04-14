d5c4b2c6ec24d63e091fdadeab9ee6bba91ce542
Karsten Bott <Karsten.Bott@emc.com>
2016-04-14 10:28:56 +0200

    added Module Description, first draft of Types

---
+++ types/siovolume.psm1
@@ -0,0 +1,22 @@
+﻿function Get-SIOVolume
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+    )
+    Begin
+    {
+    $myself = $MyInvocation.MyCommand.Name -replace "Get-SIO",""
+    $myself
+    }
+    Process
+    {
+    # (Invoke-RestMethod -Uri "$SIObaseurl/api/instances" -Headers $ScaleIOAuthHeaders -Method Get).$myself | Select-Object * -ExcludeProperty links
+    <#    
+    @{N="SDCName";E={$_.name}},
+    #>
+    }
+    End
+    {}
+}
