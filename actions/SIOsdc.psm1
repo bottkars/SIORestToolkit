1246c3ffc59068c596704252ff76e645b280bfbf
Karsten Bott <Karsten.Bott@bottnet.de>
2016-05-01 08:52:14 +0200

    added device perf test

---
+++ actions/SIOsdc.psm1
@@ -0,0 +1,56 @@
+﻿<#/api/instances/Sd
+c::{id}/action/setS
+dcPerformancePar
+ameters
+Required:
+perfProfile -
+"Default" or
+"HighPerform
+ance"
+#>
+function Set-SIOSDCPerformanceParameters
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [Alias("ID")]
+        [ValidatePattern("[0-9A-F]{16}")]$Sdcid,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='0')]
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [ValidateSet('Default','HighPerformance')]$perfProfile
+
+    )
+    begin {
+     $JSonBody = @{  
+     perfProfile = $perfProfile
+    } | ConvertTo-Json
+    }
+    process {
+    switch ($PsCmdlet.ParameterSetName)
+        {
+        "0"
+            {
+            $uri = "$SIObaseurl/api/instances/System/action/setSdcPerformanceParameters"
+            }
+        "1"
+            {
+            $uri = "$SIObaseurl/api/instances/Sdc::$Sdcid/action/setSdcPerformanceParameters"
+            }
+       }
+    try
+        {
+        Invoke-RestMethod -Uri $uri -Headers $ScaleIOAuthHeaders -Method Post -Body $JSonBody
+        }
+    catch
+        {
+        Get-SIOWebException -ExceptionMessage $_.Exception.Message
+        break
+        }
+    
+    Write-Host -ForegroundColor White "Performanceprofile set to $perfProfile"
+    }
+    end {}
+}
+
+
---
+++ actions/SIOsds.psm1
@@ -0,0 +1,134 @@
+﻿function Set-SIOSDSPerformanceParameters
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [Alias("ID")]
+        [ValidatePattern("[0-9A-F]{16}")]$SDSid,
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='0')]
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [ValidateSet('Default','HighPerformance')]$perfProfile
+
+    )
+    begin {
+     $JSonBody = @{  
+     perfProfile = $perfProfile
+    } | ConvertTo-Json
+    }
+    process {
+    switch ($PsCmdlet.ParameterSetName)
+        {
+        "0"
+            {
+            $uri = "$SIObaseurl/api/instances/System/action/setSdsPerformanceParameters"
+            }
+        "1"
+            {
+            $uri = "$SIObaseurl/api/instances/Sds::$SDSid/action/setSdsPerformanceParameters"
+            }
+       }
+    try
+        {
+        Invoke-RestMethod -Uri $uri -Headers $ScaleIOAuthHeaders -Method Post -Body $JSonBody
+        }
+    catch
+        {
+        Get-SIOWebException -ExceptionMessage $_.Exception.Message
+        break
+        }
+    
+    Write-Host -ForegroundColor White "Performanceprofile set to $perfProfile"
+    }
+    end {}
+}
+function Start-SIOSDSDevicesTest
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [Alias("ID")]
+        [ValidatePattern("[0-9A-F]{16}")]$SDSid
+        #[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='0')]
+
+    )
+    begin {
+    $JSonBody = [ordered]@{  
+pattern = "readRand"
+ioSizeKB = "8"
+totalIoSizeMB = "128"
+limitInSeconds = "10"
+    } | ConvertTo-Json
+    }
+    process {
+    switch ($PsCmdlet.ParameterSetName)
+        {
+        "0"
+            {
+            $uri = "$SIObaseurl/api/instances/System/action/setSdsPerformanceParameters"
+            }
+        "1"
+            {
+            $uri = "$SIObaseurl/api/instances/Sds::$SDSid/action/startDevicesTest"
+            }
+       }
+    try
+        {
+        Invoke-RestMethod -Uri $uri -Headers $ScaleIOAuthHeaders -Method Post -Body $JSonBody
+        }
+    catch
+        {
+        Get-SIOWebException -ExceptionMessage $_.Exception.Message
+        break
+        }
+    
+    }
+    end {}
+}
+
+<#/api/instances/Sds::{id}/
+action/startSdsNetwork
+Test#>
+
+function Start-SIOSDSNetworkTest
+{
+    [CmdletBinding(SupportsShouldProcess)]
+    Param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [Alias("ID")]
+        [ValidatePattern("[0-9A-F]{16}")]$SDSid,
+        #[Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='0')]
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,ParameterSetName='1')]
+        [Alias("NUmMesg")]
+        [ValidateRange(1,16)]$numParallelMsgs,
+                $networkTestSizeInGb,
+        $networkTestLengthInSecs
+
+    )
+    begin {
+    $JSonBody = [ordered]@{  
+numParallelMsgs= "$numParallelMsgs"
+networkTestSizeInGb =  "$networkTestSizeInGb"
+networkTestLengthInSecs = "$networkTestLengthInSecs"
+    } | ConvertTo-Json
+    }
+    process 
+    {
+    Write-Verbose $JSonBody
+    $uri = "$SIObaseurl/api/instances/Sds::$SDSid/action/startSdsNetworkTest"
+    try
+        {
+        Invoke-RestMethod -Uri $uri -Headers $ScaleIOAuthHeaders -Method Post -Body $JSonBody
+        }
+    catch
+        {
+        Get-SIOWebException -ExceptionMessage $_.Exception.Message
+        break
+        }
+    
+    }
+    end {}
+}
+
